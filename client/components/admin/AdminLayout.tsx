faac2083f3af8d629fef9be4f1318a9453358094
Kauã Fernandez <164455293+kfndz@users.noreply.github.com>
2026-08-27 21:54:45 +0000

    fix: altera o nome do menu lateral do admin para MP Vertise

--- client/components/admin/AdminLayout.tsx
+++ client/components/admin/AdminLayout.tsx
@@ -84,7 +84,7 @@ export default function AdminLayout({
           </p>
 
           <h2 className="mt-3 text-2xl font-semibold text-foreground">
-            NOXBYTE
+            MP VERTISE
           </h2>
         </div>
 
